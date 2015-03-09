f92acad1df5a1ef65e7f1e64ee57c5a3151c93cf
Zule Li <zule.li@hotmail.com>
2015-03-09 00:20:26 -0500

    First Cleaned Up

--- src/restaurant/Compute.java
+++ src/restaurant/Compute.java
@@ -1,16 +1,29 @@
 package restaurant;
 
+/* Author: Zule Li
+ * Email:zule.li@hotmail.com
+ * Last Modified Date:Mar.7,2015
+ * */
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.*;
 
-public interface Compute extends Remote 
-{
-    Vector getMenu(String resid,String catagory) throws RemoteException;
-    Boolean ask(String resid,String side,String ip) throws RemoteException;
-    String addSaleRecord(Vector saleData,String resid,String cusid) throws RemoteException;
-    void phoneRing(String resid,String phoneno,String firstname,String lastname,String midname) throws RemoteException;
-    Boolean login(String resid,String password) throws RemoteException;
-    Boolean endOp(String resid,String type,String location) throws RemoteException; 
-    Vector getHistory(String resid,String accountno,java.sql.Date begin) throws RemoteException;
+public interface Compute extends Remote {
+	Vector getMenu(String resid, String catagory) throws RemoteException;
+
+	Boolean ask(String resid, String side, String ip) throws RemoteException;
+
+	String addSaleRecord(Vector saleData, String resid, String cusid)
+			throws RemoteException;
+
+	void phoneRing(String resid, String phoneno, String firstname,
+			String lastname, String midname) throws RemoteException;
+
+	Boolean login(String resid, String password) throws RemoteException;
+
+	Boolean endOp(String resid, String type, String location)
+			throws RemoteException;
+
+	Vector getHistory(String resid, String accountno, java.sql.Date begin)
+			throws RemoteException;
 }
--- src/restaurant/p.java
+++ src/restaurant/p.java
@@ -1,14 +1,17 @@
 package restaurant;
 
-public class p 
-{
-  static boolean p=true;
-  public p()
-  {
-  }
-  public static void p(String s)
-  {
-    if(p)
-    System.out.println(s);
-  }
+/* Author: Zule Li
+ * Email:zule.li@hotmail.com
+ * Last Modified Date:Mar.7,2015
+ * */
+public class P {
+	static boolean p = true;
+
+	public P() {
+	}
+
+	public static void p(String s) {
+		if (p)
+			System.out.println(s);
+	}
 }
